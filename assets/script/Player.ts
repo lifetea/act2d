834de36d1e4e0dfe0396859e7871214bd3dd9dce
lifetea <494886251@qq.com>
2022-08-03 19:49:19 +0800

    fixs #1  攻击动画

--- assets/script/Player.ts
+++ assets/script/Player.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node, Animation, Vec3, input, Input, EventKeyboard, KeyCode } from 'cc';
+import { _decorator, Component, Node, Animation, Vec3, input, Input, EventKeyboard, KeyCode, tween, v3 } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Player')
@@ -33,6 +33,12 @@ export class Player extends Component {
                 this.accRight = true
                 this.anim.play('player-walker')
                 break;
+            case KeyCode.KEY_W:
+                this.jump()
+                break;
+            case KeyCode.KEY_J:
+                this.attack()
+                break;
         }
     }
     onKeyUp(event: EventKeyboard) {
@@ -45,22 +51,44 @@ export class Player extends Component {
                 this.accRight = false
                 this.anim.play('player-normal')
                 break;
+            case KeyCode.KEY_J:
+                // this.anim.play('player-normal')
+                break;
         }
     }
+
+    resetAnim(){
+        this.anim.play('player-normal')
+    }
+
+    jump(){
+        let pos = this.playerPos
+        //   tween(this.node).to(0.4, {position:v3(pos.x, pos.y+ 150)}, {easing:'sineOut'}).start();
+        tween(this.node).to(0.4,{position:v3(pos.x, pos.y+120, pos.z)}, {easing:'sineOut'}).start()
+    }
+
     start() {
         this.playerPos = this.node.position
     }
+
+    //攻击
+    attack(){
+        console.log('攻击')
+        this.anim.play('player-attack')
+    }
+
 
     update(deltaTime: number) {
         if (this.accLeft) {
             this.node.scale.set(-2,2,2)
             this.playerPos.x -= this.speed * deltaTime
+            this.node.setPosition(this.playerPos.x, this.playerPos.y, this.playerPos.z)
         }
         if(this.accRight){
             this.node.scale.set(2,2,2)
             this.playerPos.x += this.speed * deltaTime
+            this.node.setPosition(this.playerPos.x, this.playerPos.y, this.playerPos.z)
         }
-        this.node.setPosition(this.playerPos.x, this.playerPos.y, this.playerPos.z)
     }
 }
 
